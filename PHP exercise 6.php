3deebd175f25c1ee028da5fab114ecfc7912f6c4
EJCdgs23 <92834496+EJCdgs23@users.noreply.github.com>
2022-09-23 13:17:18 +0800

    Add files via upload

---
+++ PHP exercise 6.php
@@ -0,0 +1,18 @@
+<?php   
+ 
+$whatsit = '10'; 
+echo "Value is " . gettype($whatsit) . "\n";   
+ 
+$whatsit = 10.1; 
+echo "Value is " . gettype($whatsit) . "\n";   
+ 
+$whatsit = true; 
+echo "Value is " . gettype($whatsit) . "\n";   
+ 
+$whatsit = 10; 
+echo "Value is " . gettype($whatsit) . "\n";
+    
+$whatsit = null; 
+echo "Value is " . gettype($whatsit) . "\n";   
+ 
+?>
